7d1f6d7b9d17b04bfddb1916f9b9b617ac09ec45
dlgocks1 <cksgodl1@naver.com>
2022-12-16 16:38:40 +0900

    test : 레이스 결과 테스트 추가

--- src/test/kotlin/racingcar/domain/model/CarTest.kt
+++ src/test/kotlin/racingcar/domain/model/CarTest.kt
@@ -6,8 +6,11 @@ import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
 import org.junit.jupiter.params.provider.ValueSource
 import racingcar.domain.NumberGenerator
+import racingcar.utils.addResults
+import java.lang.StringBuilder
 
 class CarTest {
 
@@ -32,8 +35,21 @@ class CarTest {
         }
     }
 
+    @ParameterizedTest
+    @CsvSource("pobi, 3,-", "woni,4,- -", "jun,5,- -")
+    @DisplayName("자동차 결과가 정상적으로 반환되는지 테스트한다.")
+    fun raceResultTest(name: String, attemps: Int, results: String) {
+        val numberGenerator = CustomNumberGenerator()
+        val testCar = Car(name)
+        repeat(attemps) {
+            testCar.move(numberGenerator)
+        }
+        assertThat(testCar.toString()).isEqualTo("$name : $results")
+    }
+
+
     inner class CustomNumberGenerator : NumberGenerator {
-        private val numbers = mutableListOf(1, 2, 4, 5)
+        private val numbers = mutableListOf(1, 2, 4, 5, 2, 1)
         override fun generate(): Int {
             return numbers.removeAt(0)
         }
