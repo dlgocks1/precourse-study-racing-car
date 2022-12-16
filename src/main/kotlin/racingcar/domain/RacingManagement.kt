48ac4930789de43914496869b18ff54a895563e1
dlgocks1 <cksgodl1@naver.com>
2022-12-16 16:40:13 +0900

    feat : 자동차 레이싱을 관리하는 RacingManagment추가
    * 자동차 설정, 레이스, 자동차 생성 등을 수행한다.

---
+++ src/main/kotlin/racingcar/domain/RacingManagement.kt
@@ -0,0 +1,32 @@
+package racingcar.domain
+
+import racingcar.domain.model.Cars
+
+class RacingManagement {
+
+    lateinit var cars: Cars
+    private var attemps = 0
+    private var tryCount = 0
+
+    private val carFactory = CarFactory(
+        RandomNumberGenerator()
+    )
+
+    fun setCars(carNames: String) {
+        cars = carFactory.generateCar(carNames)
+    }
+
+    fun setAttemps(attempsNumber: Int) {
+        attemps = attempsNumber
+    }
+
+    fun race(): Cars {
+        cars.move()
+        tryCount += 1
+        return cars
+    }
+
+    fun isEnded(): Boolean = tryCount == attemps
+
+
+}
