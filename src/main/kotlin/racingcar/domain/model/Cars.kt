239b03738405237761efe64aa183ffe89e245d6a
dlgocks1 <cksgodl1@naver.com>
2022-12-16 16:47:04 +0900

    feat : 최종 우승자 리스트를 추출하는 기능 작성

--- src/main/kotlin/racingcar/domain/model/Cars.kt
+++ src/main/kotlin/racingcar/domain/model/Cars.kt
@@ -10,4 +10,10 @@ class Cars(private val cars: List<Car>, private val numberGenerator: NumberGener
         }
     }
 
+    fun getWinner(): List<Car> {
+        val winnerPosition = cars.maxOf { it.getPosition() }
+        cars.filter { it.getPosition() == winnerPosition }
+        return cars
+    }
+
 }
