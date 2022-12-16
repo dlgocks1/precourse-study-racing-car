a49162aac253184e19ca62294d463cf2f4a4b5f1
dlgocks1 <cksgodl1@naver.com>
2022-12-16 16:39:42 +0900

    feat : 자동차 레이싱 Controller 추가

---
+++ src/main/kotlin/racingcar/controller/racingCarController.kt
@@ -0,0 +1,38 @@
+package racingcar.controller
+
+import racingcar.domain.RacingManagement
+import racingcar.view.InputView
+import racingcar.view.OutputView
+
+class racingCarController(
+    private val inputView: InputView,
+    private val outputView: OutputView
+) {
+
+    private val racingManagement = RacingManagement()
+
+    fun racingGame() {
+        setCars()
+        setAttemps()
+        stratRacing()
+    }
+
+    private fun stratRacing() {
+        outputView.result()
+        with(racingManagement) {
+            while (isEnded()) {
+                outputView.excutionResults(race())
+            }
+        }
+    }
+
+
+    private fun setAttemps() {
+        racingManagement.setAttemps(inputView.attempsNumber())
+    }
+
+    private fun setCars() {
+        racingManagement.setCars(inputView.carNames())
+    }
+
+}
