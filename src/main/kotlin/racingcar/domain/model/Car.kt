6af31d4173f65d59790d38695272c1145104d2b8
dlgocks1 <cksgodl1@naver.com>
2022-12-16 15:27:32 +0900

    feat : 자동차 객체에 getName()추가

--- src/main/kotlin/racingcar/domain/model/Car.kt
+++ src/main/kotlin/racingcar/domain/model/Car.kt
@@ -8,10 +8,13 @@ class Car(private val name: String) {
         validateName(name)
     }
 
+    fun getName(): String = name
+
     private fun validateName(name: String) {
         require(name.length in 1..5) {
             "자동차의 이름은 1글자 이상 5자 이하여야 합니다."
         }
     }
+
 
 }
