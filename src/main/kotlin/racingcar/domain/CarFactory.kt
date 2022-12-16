8c82c96d2b15c4512c93c147a1df4b852db22f19
dlgocks1 <cksgodl1@naver.com>
2022-12-16 16:03:33 +0900

    fix : 클래스 미작성 수정

--- src/main/kotlin/racingcar/domain/CarFactory.kt
+++ src/main/kotlin/racingcar/domain/CarFactory.kt
@@ -3,7 +3,7 @@ package racingcar.domain
 import racingcar.domain.model.Car
 import racingcar.domain.model.Cars
 
-class CarFactory(private val randomNumberGenerator) {
+class CarFactory(private val randomNumberGenerator: NumberGenerator) {
 
     fun generateCar(carNames: String): Cars {
         return Cars(
