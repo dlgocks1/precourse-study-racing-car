ff4019e22df0776378a45bd3a21677b35a164670
dlgocks1 <cksgodl1@naver.com>
2022-12-16 23:31:19 +0900

    feat : Controller와 연결

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,5 +1,17 @@
 package racingcar
 
+import racingcar.controller.RacingCarController
+import racingcar.view.InputView
+import racingcar.view.OutputView
+
 fun main() {
-    TODO("프로그램 구현")
+    try {
+        val racingCarController = RacingCarController(
+            inputView = InputView(),
+            outputView = OutputView()
+        )
+        racingCarController.racingGame()
+    } catch (e: Exception) {
+        println(e.message)
+    }
 }
